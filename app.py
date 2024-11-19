f2f744cde59680767e84924e20f8fe05a5df6182
Balachandru.S <balachandrus2006@gmail.com>
2024-11-19 14:49:53 +0530

    app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if st.session_state.submitted:
     student_info = st.button("Student Information")
     student = st.session_state.student
     st.sidebar.image(student.img, width=200)
-    # st.sidebar.markdown(f"<h5>{name .title()}</h5> ",unsafe_allow_html=True)
+    st.sidebar.markdown(f"<h5>{name .title()}</h5> ",unsafe_allow_html=True)
     if student_info:
         if st.session_state.student:
             #student = st.session_state.student
