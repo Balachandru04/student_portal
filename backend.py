d45eef9ada0d57f7d47e76675a94898eaf1b9c22
Balachandru.S <balachandrus2006@gmail.com>
2024-11-19 13:44:41 +0530

    backend.py

--- backend.py
+++ backend.py
@@ -11,8 +11,8 @@ class BALA:
         self.batch=1
         self.instution_name="SkillEdge Coaching"
         self.img=Image.open("assets/bala.png")
-        self.phone=8122687009
-        self.mail_id="balachandrusingaram@gmail.com"
+        self.phone=8164972587
+        self.mail_id="bala143@gmail.com"
 class CHANDRU:
     def __init__(self):
         self.attendance=65
@@ -24,8 +24,8 @@ class CHANDRU:
         self.batch=1
         self.instution_name="ABC Coaching"
         self.img=Image.open("assets/chandru.png")
-        self.phone=9786424873
-        self.mail_id="balachandrus2006@gmail.com"
+        self.phone=9786798218
+        self.mail_id="chandrus20@gmail.com"
 
 class MOHAN:
     def __init__(self):
@@ -38,8 +38,8 @@ class MOHAN:
         self.batch=1
         self.instution_name="SkillEdge Coaching"
         self.img=Image.open("assets/mohan.png")
-        self.phone=9342797914
-        self.mail_id="mohans0110@gmail.com"
+        self.phone=9347812264
+        self.mail_id="mohans010@gmail.com"
 
 class ASHARAJA:
     def __init__(self):
@@ -52,8 +52,8 @@ class ASHARAJA:
         self.batch=1
         self.instution_name="SkillEdge Coaching"
         self.img=Image.open("assets/asharaja.png")
-        self.phone=9942890973
-        self.mail_id="asharaja143@gmail.com"
+        self.phone=9974954136
+        self.mail_id="asha143@gmail.com"
 
 class DEVA:
     def __init__(self):
@@ -66,7 +66,7 @@ class DEVA:
         self.batch=1
         self.instution_name="SkillEdge Coaching"
         self.img=Image.open("assets/bala.png")
-        self.phone=8122687009
+        self.phone=9786798218
         self.mail_id="deva185@gmail.com"
 
 class SHALINI:
@@ -80,8 +80,8 @@ class SHALINI:
         self.batch=1
         self.instution_name="SkillEdge Coaching"
         self.img=Image.open("assets/bala.png")
-        self.phone=8122687009
-        self.mail_id="Shalini125@gmail.com"
+        self.phone=9786497235
+        self.mail_id="Shalu125@gmail.com"
 
 
 class THAZIM:
@@ -95,5 +95,5 @@ class THAZIM:
         self.batch=1
         self.instution_name="SkillEdge Coaching"
         self.img=Image.open("assets/bala.png")
-        self.phone=8122687009
-        self.mail_id="Shalini125@gmail.com"
+        self.phone=9786247910
+        self.mail_id="thaz125@gmail.com"
